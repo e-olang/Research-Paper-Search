d96e65c53817fd3862055462514094a81dbdeb80
Emmanuel Olang <64926283+e-olang@users.noreply.github.com>
2023-02-28 13:05:59 +0300

    HF Stremalit App v1

---
+++ Streamlit App/app.py
@@ -0,0 +1,67 @@
+import pickle
+import json
+import streamlit as st
+import pandas as pd
+
+
+#load files/resources using pickle
+with open('resources/titles.pkl', 'rb') as f:
+    titles = pickle.load(f)
+
+with open('resources/embeddings.pkl', 'rb') as f:
+    embeddings = pickle.load(f)
+
+with open('resources/authors.pkl', 'rb') as f:
+    authors = pickle.load(f)
+
+with open('resources/years.pkl', 'rb') as f:
+    years = pickle.load(f)
+
+with open('resources/summary.pkl', 'rb') as f:
+    summary = pickle.load(f)
+
+index = pickle.load(open('resources/index.sav' , 'rb'))
+
+model = pickle.load(open('resources/model.sav' , 'rb'))
+
+
+# query fucntion
+def query(query_text, k=5):
+    enc = model.encode([query_text])
+    D, I = index.search(enc, k)
+
+    resutls = []
+    for i in range(k):
+        resutls.append(
+            {
+                "Title" : titles[I[0][i]],
+                "Author" : authors[I[0][i]],
+                "Year" : years[I[0][i]],
+                "Summary" : summary[I[0][i]]
+                
+            }
+        )
+    
+    return resutls
+
+
+def main():
+    # Streamlit App
+    st.title("Search Engine")
+
+    query_text = st.text_input("Enter your query")
+
+    if st.button("Search"):
+        results = query(query_text)
+        
+        for result in results:
+            st.write(result["Title"])
+            st.write(result["Author"])
+            st.write(result["Year"])
+            
+            # justified text for summary
+            st.markdown(f"<p style='text-align: justify;'>{result['Summary']}</p>", unsafe_allow_html=True)
+            st.write("--" * 50)
+
+if __name__ == "__main__":
+    main()
