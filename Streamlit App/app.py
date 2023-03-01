7223f5f169c3544da16bdee590e9281990ad30ba
J. Olang <64926283+e-olang@users.noreply.github.com>
2023-03-01 11:38:51 +0300

    Added Google URL search

--- Streamlit App/app.py
+++ Streamlit App/app.py
@@ -20,29 +20,32 @@ with open('resources/years.pkl', 'rb') as f:
 with open('resources/summary.pkl', 'rb') as f:
     summary = pickle.load(f)
 
-index = pickle.load(open('resources/index.sav' , 'rb'))
+with open('resources/titles_urls.pkl', 'rb') as f:
+    title_urls = pickle.load(f)
 
+
+index = pickle.load(open('resources/index.sav' , 'rb'))
 model = pickle.load(open('resources/model.sav' , 'rb'))
 
 
-# query fucntion
+# Query Function
 def query(query_text, k=5):
     enc = model.encode([query_text])
     D, I = index.search(enc, k)
 
-    resutls = []
+    results = []
     for i in range(k):
-        resutls.append(
+        results.append(
             {
                 "Title" : titles[I[0][i]],
                 "Author" : authors[I[0][i]],
                 "Year" : years[I[0][i]],
-                "Summary" : summary[I[0][i]]
-                
+                "Summary" : summary[I[0][i]],
+                "Title URL" : title_urls[I[0][i]]
             }
         )
     
-    return resutls
+    return results
 
 
 def main():
@@ -55,12 +58,14 @@ def main():
         results = query(query_text)
         
         for result in results:
-            st.write(result["Title"])
-            st.write(result["Author"])
-            st.write(result["Year"])
+            st.write("Title:   " + result["Title"])
+            st.write("Author:   " + result["Author"])
+            st.write("Year Published:   " +  str(result["Year"]))
             
             # justified text for summary
+            st.write("Abstract")
             st.markdown(f"<p style='text-align: justify;'>{result['Summary']}</p>", unsafe_allow_html=True)
+            st.markdown(f"<a href='{result['Title URL']}'>Google Search</a>", unsafe_allow_html=True)
             st.write("--" * 50)
 
 if __name__ == "__main__":
